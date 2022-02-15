368c0ee91142a3843370b1d3202ef003045b753f
AkashAnoly-uk <99469278+AkashAnoly-uk@users.noreply.github.com>
2022-02-15 12:37:16 +0530

    settings file
    updated

--- awesome_website/awesome_website/settings.py
+++ awesome_website/awesome_website/settings.py
@@ -25,25 +25,7 @@ SECRET_KEY = 'django-insecure-1h(i1h)qvs1!st!alu!la)u&#_hlgup2nwrk=&x7(*y$0$x#fd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
 
-# from pathlib import Path
-
-# import environ
-# import os
-# env = environ.Env()
-# environ.Env.read_env()
-# # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-# # Quick-start development settings - unsuitable for production
-# # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env('SECRET_KEY')
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['']
 
@@ -172,6 +154,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 
 # https://www.javahelps.com/2018/10/install-mysql-with-phpmyadmin-on-ubuntu.html
